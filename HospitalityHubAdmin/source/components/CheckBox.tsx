af498c1804e14f31773f71738c1d9a32dcc82f6b
megs995 <meghna.malhotra@appinventiv.com>
2023-10-27 15:36:20 +0530

    fix:checkbox in darkmode

--- HospitalityHubAdmin/source/components/CheckBox.tsx
+++ HospitalityHubAdmin/source/components/CheckBox.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { useColorScheme } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+
 const Checkbox = () => {
+  const colorScheme = useColorScheme();
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
+  const checkboxColor = colorScheme === 'dark' ? 'black' : 'black';
+  const borderColor = colorScheme === 'dark' ? 'black' : 'black';
 
   return (
     <CheckBox
+      boxType="square"
+      tintColors={{ true: checkboxColor, false: checkboxColor }} 
+      onTintColor={borderColor} //for ios
+      onFillColor={borderColor} 
       disabled={false}
       value={toggleCheckBox}
-      onValueChange={newValue => setToggleCheckBox(newValue)}
+      onValueChange={(newValue) => setToggleCheckBox(newValue)}
     />
   );
 };
